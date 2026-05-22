23b40530a3cec15d4628b41ff7de2eb65898d6e4
VTGTHB <volodymyrt3@nycstudents.net>
2026-05-22 09:53:52 -0400

    Update Solution.java

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,11 +1,19 @@
 public int moreHistoryThanMathAbsences()
 {
 int TrueStudents = 1
-
-
-
-
-
-
+for (CourseRecord historyRecord : historyList)
+    {
+        String historyID = historyRecord.getStudentID();
+        for (CourseRecord mathRecord : mathList)
+        {
+            if (historyID.equals(mathRecord.getStudentID()))
+            {
+                if (historyRecord.getAbsences() > mathRecord.getAbsences())
+                {
+                    TrueStudents++;
+                }
+            }
+        }
+    }
 return TrueStudents;
 }
